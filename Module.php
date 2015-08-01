2dfa71781dd5a9847a5cdc6a2d39bbc43d8d6b8b
Roman Zhuravlev <zhuravljov@gmail.com>
2015-08-01 03:11:31 +0300

    Access control

--- Module.php
+++ Module.php
@@ -2,12 +2,25 @@
 
 namespace zhuravljov\yii\rest;
 
+use Yii;
 use yii\base\BootstrapInterface;
 use yii\web\Application;
+use yii\web\ForbiddenHttpException;
 
 class Module extends \yii\base\Module implements BootstrapInterface
 {
+    /**
+     * @inheritdoc
+     */
     public $controllerNamespace = 'zhuravljov\yii\rest\controllers';
+    /**
+     * @var array the list of IPs that are allowed to access this module.
+     * Each array element represents a single IP filter which can be either an IP address
+     * or an address with wildcard (e.g. 192.168.0.*) to represent a network segment.
+     * The default value is `['127.0.0.1', '::1']`, which means the module can only be accessed
+     * by localhost.
+     */
+    public $allowedIPs = ['127.0.0.1', '::1'];
 
     /**
      * @inheritdoc
@@ -19,5 +32,49 @@ class Module extends \yii\base\Module implements BootstrapInterface
                 $this->id => $this->id . '/default/index',
             ], false);
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+
+        if (Yii::$app instanceof Application && !$this->checkAccess()) {
+            throw new ForbiddenHttpException('You are not allowed to access this page.');
+        }
+
+        $this->resetGlobalSettings();
+
+        return true;
+    }
+
+    /**
+     * Resets potentially incompatible global settings done in app config.
+     */
+    protected function resetGlobalSettings()
+    {
+        if (Yii::$app instanceof Application) {
+            Yii::$app->assetManager->bundles = [];
+        }
+    }
+
+    /**
+     * @return boolean whether the module can be accessed by the current user
+     */
+    protected function checkAccess()
+    {
+        $ip = Yii::$app->getRequest()->getUserIP();
+        foreach ($this->allowedIPs as $filter) {
+            if ($filter === '*' || $filter === $ip || (($pos = strpos($filter, '*')) !== false && !strncmp($ip, $filter, $pos))) {
+                return true;
+            }
+        }
+        Yii::warning('Access to REST Client is denied due to IP address restriction. The requested IP is ' . $ip, __METHOD__);
+
+        return false;
     }
 }
